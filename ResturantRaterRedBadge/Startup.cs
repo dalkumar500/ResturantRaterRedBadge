1b6dd643a58c7437965061d0a3a5e45a679c27a5
dalkumar500 <dalkumar500@gmail.com>
2021-10-05 19:24:02 -0400

    Add project files.

---
+++ ResturantRaterRedBadge/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResturantRaterRedBadge.Startup))]
+namespace ResturantRaterRedBadge
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
